2f5935b4379eb0e72e1f844676214d68e612cef5
lovem <lovem@wchwang>
2025-08-25 21:49:41 +0900

    master commit

--- src/gitTestJava/Main.java
+++ src/gitTestJava/Main.java
@@ -23,6 +23,8 @@ public class Main {
         System.out.println("This is a new feature branch4");
         System.out.println("This is a new feature branch5");
         
+        System.out.println("This is a new feature master1");
+        
         
         try {
             // 1. XML 파싱
