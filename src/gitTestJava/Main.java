8129508f9ecdc5d73e9438c970e1535fdb193d0c
lovem <lovem@wchwang>
2025-08-25 21:53:06 +0900

    brunch commit

--- src/gitTestJava/Main.java
+++ src/gitTestJava/Main.java
@@ -24,6 +24,8 @@ public class Main {
         System.out.println("This is a new feature branch5");
         System.out.println("This is a new feature branch6");
         
+        System.out.println("This is a new feature function branch7");
+        
         
         try {
             // 1. XML 파싱
